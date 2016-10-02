f45207ddc7565a885968c3ea0116243ca15169ca
enoughtime@gmail.com <enoughtime@gmail.com>
2016-10-02 11:47:52 -0400

    Checkpoint blocipedia 03 User Sign In and Out: Completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,7 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def edit
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+ # def email_required?
+ #   false
+ # end
+ # 
+ # def email_changed?
+ #   false
+ # end
+
 end
